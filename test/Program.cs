9650135282a6d0e30336c188713db5d88df4d23b
Владислав Дроздов <90255521+prplV@users.noreply.github.com>
2021-10-28 15:36:34 +0300

    pr_1
    pr_1 strings... a lot of strings

---
+++ test/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 0;
+            double b = 0;
+            double c = 0;
+
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            c = Convert.ToDouble(Console.ReadLine());
+
+            double d = b*b - 4 * a * c;
+
+            if (d > 0)
+            {
+                Console.WriteLine($"\n x1 = {(-b + Math.Sqrt(d)) / (2 * a)} \n x2 = {(-b - Math.Sqrt(d)) / (2 * a)}");
+            }
+            else
+            {
+                if (d == 0)
+                {
+                    Console.WriteLine($"\n x = {-b / (2*a)}");
+                }
+                else
+                {
+                    Console.WriteLine("Net korney!");
+                }
+            }
+        }
+    }
+}

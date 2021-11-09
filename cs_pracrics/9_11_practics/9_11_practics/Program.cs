92bf95a737cbfb7911f9c6d058e4e427310d7450
Владислав Дроздов <90255521+prplV@users.noreply.github.com>
2021-11-09 12:13:26 +0300

    программа с занятия за 9.11
    задача на полукалькулятор, решённая через методы

---
+++ cs_pracrics/9_11_practics/9_11_practics/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _9_11_practics
+{
+    class Program
+    {
+        static int Math(int a, int b, char c)
+        {
+            if (c == '+')
+            {
+                return a + b;
+            }
+            else if (c == '-')
+            {
+                return a - b;
+            }
+            else if (c == '*')
+            {
+                return a * b;
+            }
+            else return 0;
+        }
+
+        static void Main()
+        {
+            Console.Write("1 - ");
+            int q = Convert.ToInt32(Console.ReadLine());
+            Console.Write("2 - ");
+            int w = Convert.ToInt32(Console.ReadLine());
+            Console.Write("math operator - ");
+            char s = Convert.ToChar(Console.ReadLine());
+            if (Math(q, w, s) != 0) {
+                Console.WriteLine(Math(q, w, s));
+            }
+            else
+            {
+                Console.WriteLine("Error!");
+            }
+        }
+    }
+}

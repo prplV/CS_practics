50a6463d2f5ac79ab03d210fa7b8945ba2d9bdd1
Владислав Дроздов <90255521+prplV@users.noreply.github.com>
2021-11-23 18:12:43 +0300

    practica 23 11
    practica 23 11 (ne vse progy)

---
+++ cs_pracrics/23_11prpr/21_11/21_11/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace prpr_23_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите кол-во строк массива");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите кол-во столбцов массива");
+            int b = int.Parse(Console.ReadLine());
+
+            int[,] array = new int[a, b];
+
+            for (int i = 0; i < a; i++) 
+            {
+                for (int j = 0; j < b; j++) 
+                {
+                    array[i, j] = Console.Read();
+                }
+            }
+
+            Obr(array, a, b);
+        }
+
+        static void Obr(int[,] mas, int a, int b) 
+        {
+            int sum = 0;
+            int pr = 1;
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < b; j++)
+                {
+                    if (i == j)
+                    {
+                        sum += mas[i, j];
+                    }
+                    else if (i + j == b - 1)
+                    {
+                        pr *= mas[i, j];
+                    }
+                }
+
+            }
+            Console.WriteLine(sum);
+            Console.WriteLine(pr);
+        }
+    }
+}

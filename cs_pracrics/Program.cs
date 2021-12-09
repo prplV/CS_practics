22523a660215ea95492891d19e2512634a708d40
Владислав Дроздов <90255521+prplV@users.noreply.github.com>
2021-12-09 15:21:17 +0300

    <list> + [SERIALIZABLE]
    <list> + [SERIALIZABLE]<list> + [SERIALIZABLE]<list> + [SERIALIZABLE]<list> + [SERIALIZABLE]<list> + [SERIALIZABLE]<list> + [SERIALIZABLE]<list> + [SERIALIZABLE]<list> + [SERIALIZABLE]<list> + [SERIALIZABLE]<list> + [SERIALIZABLE]<list> + [SERIALIZABLE]<list> + [SERIALIZABLE]<list> + [SERIALIZABLE]<list> + [SERIALIZABLE]<list> + [SERIALIZABLE]<list> + [SERIALIZABLE]<list> + [SERIALIZABLE]<list> + [SERIALIZABLE]<list> + [SERIALIZABLE]<list> + [SERIALIZABLE]<list> + [SERIALIZABLE]<list> + [SERIALIZABLE]<list> + [SERIALIZABLE]<list> + [SERIALIZABLE]<list> + [SERIALIZABLE]<list> + [SERIALIZABLE]<list> + [SERIALIZABLE]<list> + [SERIALIZABLE]

--- cs_pracrics/Program.cs
+++ cs_pracrics/Program.cs
@@ -1,193 +1,144 @@
 ﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using System.Collections;
+using System.Collections.Generic;
 
-namespace cs_pracrics
+namespace practica
 {
+    /*
+    [Serializable]
+    public class Calc
+    {
+        public int i { get; set; }
+        public int b { get; set; }
+
+        public Calc() {
+        
+        }
+        public Calc(int num1, int num2)
+        {
+            i = num1;
+            b = num2;
+        }
+    }
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            /*
-            string s1 = "a";
-            string s2 = null;
+            Calc nabor1 = new Calc(13, 21);
 
-            string s3 = new String('a', 6); //result - 'aaaaaa'
-            string s4 = new String(new char[]{'w','o','r','l', 'd'});
-            */
+            XmlSerializer formatter = new XmlSerializer(typeof(Calc));
 
-
-            /*
-            string s = "МИРЭА";
-            Console.WriteLine(s[1]); //И
-            //1
-            for (int i = 0; i < s.Length; i++)
+            using (FileStream fl = new FileStream("1.xml", FileMode.Append))
             {
-                Console.WriteLine(s[i]);
+                formatter.Serialize(fl, nabor1);
             }
-            //2
-            foreach(char q in s)
-            {
-                Console.WriteLine(q);
-            }
-            string s0 = null;
-            for (int i = 0; i < s.Length; i++)
-            {
-                s0 += s[i] + " ";
-            }
-            Console.WriteLine(s0);
-            */
-
-
-            /*Конкатенация №1
-            string a = "Hello";
-            string b = "World";
-            string c = a + " " + b;
-            string s = String.Concat(c, " !!!");
-
-            Console.WriteLine(s);
-            */
-
-
-            /*Конкатенация №2
-            string s1 = "apple";
-            string s2 = "a day";
-            string s3 = "keeps";
-            string s4 = "a doctor";
-            string s5 = "away";
-            string[] values = new string[] { s1, s2, s3, s4, s5 };
-
-            string s6 = String.Join(" ", values);
-            Console.WriteLine(s6);
-            */
-
-
-            //Сравнение строк
-            /*
-            string s1 = null;
-            s1 = Convert.ToString(Console.ReadLine());
-            string s2 = null;
-            s2 = Convert.ToString(Console.ReadLine());
-            int result = String.Compare(s1, s2);
-            if (result < 0)
-            {
-                Console.WriteLine("1 -> 2");
-                string s3 = s1 + " " + s2;
-                Console.WriteLine(s3);
-            }
-            else if(result > 0) 
-            {
-                Console.WriteLine("2 -> 1");
-                string s3 = s2 + " " + s1;
-                Console.WriteLine(s3);
-            }
-            else
-            {
-                Console.WriteLine("1 == 2");
-                string s3 = s2 + " " + s1;
-                string s4 = s1 + " " + s2;
-                Console.WriteLine($"{s3} ili {s4}");
-                
-            }
-            */
-
-
-            //Поиск в строке
-            /*
-            string s1 = "Hello World";
-            char s2 = 'o';
-            int index = s1.IndexOf(s2);
             
-            Console.WriteLine(index);
+        }
+    }
+    */
 
-            string s3 = "Wor";
-            int index2 = s1.IndexOf(s3);
+    /*
+    class Program 
+    {   
+        static void Main()
+        {
+            
+            List<string> countries = new List<string>() { "Russia", "France", "Germany", "China"};
+            countries.Add("USA");
+            countries.RemoveAt(3);
+            //
+            
+            
+        }
+    }
+    */
 
-            Console.WriteLine(index2);
+    class Program 
+    {
+        static void Main() 
+        {
+            List<string> polz = new List<string>();
+            char o = ' ';
 
-            int y = 0;
-            for (int i = s1.Length-1; i >= 0; i--)
+            while (true)
             {
-                if (s1[i] == 'o')
+                Console.WriteLine("Enter operation (1 - add polz, 2 - search polz, 3 - delete polz, 4 - show all users, 0 - exit)");
+                try
                 {
-                    y = i;
+                    o = Convert.ToChar(Console.ReadLine());
+                }
+                catch (Exception exp) {
+                    Console.WriteLine($"***********Exception:***********{'\n'}{exp}");
+                }
+                if (o == '1')
+                {
+                    Console.WriteLine("Enter name, fname and lname of new user splitted by char ' ': ");
+                    string new_polz = Console.ReadLine();
+                    polz.Add(new_polz);
+                    Console.WriteLine("***********New user was succesfully added!***********");
+                }
+                else if (o == '2')
+                {
+                    Console.WriteLine("Enter name, fname and lname of the user who you're looking for:");
+                    string find = Console.ReadLine();
+                    string result = null;
+                    foreach (string pol in polz)
+                    {
+                        if (pol.Contains(find))
+                        {
+                            result = pol;
+                        }
+                    }
+                    if (result == "")
+                    {
+                        Console.WriteLine("This user doesn't exist");
+                    }
+                    else {
+                        Console.WriteLine($"***********Your user exists***********{'\n'}Full name = {result}");
+                    }
+                }
+                else if (o == '3')
+                {
+                    Console.WriteLine("Enter name, fname and lname of the user who you'd like to delete:");
+                    string delete = Console.ReadLine();
+                    int result = 0;
+                    foreach (string user in polz) {
+                        if (user.Contains(delete) == false)
+                        {
+                            result++;
+                        }
+                        else {
+                            string outsider = user;
+                            polz.RemoveAt(result);
+                            Console.WriteLine($"***********Succesfull operation!***********");
+                            Console.WriteLine($"User {outsider} was succesfully deleted!");
+                            break;
+                        }
+
+                    }
+                    Console.WriteLine("This user doesn't exist");
+                }
+                else if (o == '4')
+                {
+                    int counter = 1;
+                    Console.WriteLine("***********Users:***********");
+                    foreach (string user in polz)
+                    {
+                        Console.WriteLine($"{counter}) {user}");
+                    }
+                    Console.WriteLine("***********End of array***********");
+                }
+                else if (o == '0') 
+                {
                     break;
-                } 
+                }
+                else
+                {
+                    Console.WriteLine("*********************************");
+                }
             }
-            Console.WriteLine(y);
-            */
-
-
-            //Обрезка строки Trim
-            /*
-            string text = " hello world ";
-            text = text.Trim(); //hello world
-            text = text.Trim(new char[] {'h', 'e', 'l', 'o', ' '}); //world
-            Console.WriteLine(text);
-            */
-
-
-            //Обрезка строки Substring
-            /*
-            string text = "lovesomemama";
-            text = text.Substring(2); // vesomemama
-            text = text.Substring(0, text.Length - 2); //vesomema
-
-            Console.WriteLine(text);
-            */
-
-
-            //Разделение строк (Split)
-            /*
-            string text = "that's why we fell apart";
-            string[] words = text.Split(new char[] { ' ' });
-
-            foreach (string q in words)
-            {
-                Console.WriteLine(q);
-                //that's
-                //why
-                //we
-                //fell
-                //apart
-            }
-            */
-
-
-            //Вставка
-            /*
-            string text1 = "we can overcome it";
-            string text2 = "simply ";
-
-            text1 = text1.Insert(7, text2);
-            Console.WriteLine(text1);
-            */
-
-
-            //Удаление строк (Remove)
-            /*
-            string text = "What a beitiful day!";
-            int a = text.Length-1;
-            text = text.Remove(a);
-            Console.WriteLine(text); // What a beitiful day
-            */
-
-
-            //Замена (Replace)
-            /*
-            string text = "what a beautiful day!";
-
-            text = text.Replace("a beautiful", "an awful");
-            Console.WriteLine(text);
-            */
-
-
-            //Смена регистра (ToUpper() / ToLower())
-            /*
-            string text = "You know, sometimes i can't simply realize what you're talking about...";
-
-            Console.WriteLine(text.ToLower());
-            Console.WriteLine(text.ToUpper());
-
-            */
         }
     }
 }
